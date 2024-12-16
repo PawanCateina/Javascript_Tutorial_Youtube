ab88b70079476fcfe31e855eb6b2e2e570481d12
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2024-12-16 17:49:29 +0000

    01variables

---
+++ 01basics/01variables.js
@@ -0,0 +1,23 @@
+//variables
+const pi=3.14
+
+//console.log(pi)
+
+let email="pawan@gmail.com"
+
+var account = "12345"
+
+accountCity = "jaipur"
+
+let accountState;
+email="pawanjinks@cateina.com"
+account="211211212"
+accountCity="Mumbai"
+
+console.table([email,account,accountCity,accountState]);
+
+
+//pi=5
+console.log(pi);
+
+/*Prefer not to use var because of scope and functional scope issue*/
