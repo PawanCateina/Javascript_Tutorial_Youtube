1273019173115be578eb3d29f0187b280a253444
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2024-12-17 15:18:07 +0000

    02datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code has newer version
+
+//code readability is must
+
+/*Primitive DatTypes - Built in Datatypes in Javascript
+1.Numbers
+2.Booleans
+3.Strings
+4.BigInt
+5.null
+6.undefined
+7.Symbol
+
+Non Primitive DataTypes
+1.Objects
+2.Arrays
+*/
+
+let n1=1
+let name="John"
+let isLoogedIn= false
+let accountNumber = null
+let accountState
+
+console.table([n1,name,isLoogedIn,accountNumber,accountState])
+console.log(typeof name);
