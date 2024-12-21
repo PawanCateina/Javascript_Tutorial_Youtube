30597a89698c0aef2d5967cc74d7963acd3a0bb6
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2024-12-21 16:26:01 +0000

    03conversions

---
+++ 01basics/03conversion.js
@@ -0,0 +1,35 @@
+/*
+let score=false
+console.log(typeof score);//console.log(typeof (score))
+
+let valueInNumber=Number(score)
+console.log(valueInNumber);//NaN - Not a Number
+console.log(typeof valueInNumber);
+*/
+
+//33 > number
+//"33abc" > NaN
+//null > NaN
+//true > 1
+//false > 0
+
+
+let loggedIn=10
+
+let valueConvert = Boolean(loggedIn)
+console.log(valueConvert);
+console.log(typeof valueConvert);
+
+//1 > true; 0 > false
+//"john" > true
+
+
+
+
+let num1 = 20
+let string1 = String(num1)
+console.log(typeof string1);
+
+
+
+
