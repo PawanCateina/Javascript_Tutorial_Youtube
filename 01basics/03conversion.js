744428d1a61f33a3129e83b7aa1717bc2d1dd9b5
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2024-12-23 10:58:09 +0000

    03converions_operations_in_js

--- 01basics/03conversion.js
+++ 01basics/03conversion.js
@@ -5,7 +5,7 @@ console.log(typeof score);//console.log(typeof (score))
 let valueInNumber=Number(score)
 console.log(valueInNumber);//NaN - Not a Number
 console.log(typeof valueInNumber);
-*/
+
 
 //33 > number
 //"33abc" > NaN
@@ -29,6 +29,35 @@ console.log(typeof valueConvert);
 let num1 = 20
 let string1 = String(num1)
 console.log(typeof string1);
+
+
+*/
+
+//Operations
+
+let value =3
+let negValue =-value
+console.log(negValue);
+
+
+//basic math operations
+//addition,subtraction,division,multliplication,power,modulus
+// etc
+
+
+//string concateination
+let str1="Hello"
+let str2=" John"
+
+console.log(str1+str2);
+console.log("1"+2);
+console.log("1"+2+2);
+console.log("1"+2+2);
+console.log(2+2+"1");
+console.log(false+1);
+console.log(+"");
+
+
 
 
 
