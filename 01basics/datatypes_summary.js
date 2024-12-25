996da2cf1af9c6e3a3540dda08063e0846b74437
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2024-12-25 11:37:10 +0000

    DataTypes_Summary

---
+++ 01basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+/* 
+Primitive DataTypes
+1.String
+2.Boolean
+3.Symbol
+4.BigInt
+5.Number
+6.Null
+7.Undefined
+
+
+Non Primitive Datatype/Reference Type
+
+1.Arrays
+2.Objects
+3.Functions
+
+Javascript is dynamically typed language,because variables can hold different types during runtime 
+
+//typeof - tp get the type of a variable
+*/
