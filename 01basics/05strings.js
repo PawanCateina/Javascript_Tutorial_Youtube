2171f22576a7b25248d6b1120801cb3876577eda
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2024-12-28 14:20:31 +0000

    06num_maths.js

--- 01basics/05strings.js
+++ 01basics/05strings.js
@@ -1,4 +1,24 @@
 //Strings
 
 let name="John"
-console.log(name);
+let repoCount = 5
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // String Interpolation
+
+//another way to write a string
+//String is an Object
+const stuName = new String('Virat Kohli')
+const stuName1 = '    Rohit    '
+console.log(stuName);
+console.log(stuName[0]);
+console.log(stuName.length);
+console.log(stuName.toUpperCase());
+console.log(stuName.toLowerCase());
+console.log(stuName.charAt(8));
+console.log(stuName.indexOf('h'));
+console.log(stuName.slice(0,8));
+console.log(stuName1);
+console.log(stuName1.trim());
+const url = "http://johnwick/john%20wick/"
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('john'));
---
+++ 01basics/06nums&maths.js
@@ -0,0 +1,36 @@
+//Numbers and Maths
+
+/*
+const score = 10
+const count = new Number(100.200)
+
+//console.log(score);
+console.log(count.toString());
+console.log(count.toFixed(1));
+
+//console.log(typeof count.toString());
+
+// const hundreds = 100000
+
+console.log(hundreds.toLocaleString('en-IN'));
+*/
+
+// =============================== Maths ======================================
+
+// console.log(Math.random()*10);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+
+// console.log(Math.sqrt(100));
+
+// console.log(Math.max(100,200,132,904,563));
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
