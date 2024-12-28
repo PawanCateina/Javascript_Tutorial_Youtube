003c985a204d5892fe4587e860c49a4c6987b5e4
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2024-12-28 14:44:31 +0000

    07date_time

---
+++ 01basics/07date_time.js
@@ -0,0 +1,16 @@
+//Dates
+
+let date = new Date()
+// let myCreatedDate = new Date(2025,11,21)
+let myCreatedDate = new Date("01-01-2024")
+// console.log(`Date ${date}`);
+// console.log(`to date string ${date.toDateString()}`);
+// console.log(`to ISO String ${date.toISOString()}`);
+// console.log(`to JSON ${date.toJSON()}`);
+// console.log(`to Locale String ${date.toLocaleString()}`);
+// console.log(`to String ${date.toString()}`);
+// console.log(typeof date);
+
+console.log(myCreatedDate.toLocaleString());
+
+
