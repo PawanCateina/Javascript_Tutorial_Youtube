bb58b39705a7fb46d37ec66462bbe0b54fae0416
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2024-12-31 12:15:53 +0000

    03basics_functions

---
+++ 03basics/01function.js
@@ -0,0 +1,26 @@
+// function greet(){
+//     console.log("Hello World");
+    
+// }
+
+// greet//reference
+//greet()//Execution
+
+
+// function addition(n1,n2){ //this is parameters
+//     console.log(n1+n2);   
+// }
+
+
+
+// const result = addition(2,4)
+// console.log(result);
+
+
+function loginUserMessage(user="sam"){
+    return `${user} just logged in`
+}
+
+console.log(loginUserMessage())
+// console.log(loginUserMessage());
+
