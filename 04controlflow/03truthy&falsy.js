a6782ad9a8dd7409330706b6bf0d85b16c4d747c
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2025-01-18 09:22:49 +0000

    02switch&truthyfalsey_value

---
+++ 04controlflow/03truthy&falsy.js
@@ -0,0 +1,48 @@
+// Truthy and falsy Values
+
+const userEmail = []
+
+// if (userEmail) {
+//     console.log("User has a email");
+    
+// } else {
+//     console.log("User doesnt a email");
+    
+// }
+
+// falsy values
+// false,0,-0,undefined,null,NaN,BigInt 0n,""
+
+// truthy values
+// [],{},"0",'false'," ",function (){}
+
+// if (userEmail.length===0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length===0) {
+//     console.log("Object is empty");
+    
+// }
+
+// nullish coalesching operator (??) null and undefined
+
+// let val2 = 2 ?? 5
+// // let val1 = null ?? 10
+// let val1 = undefined ?? 15
+
+// let val3 = null ?? 10 ?? 5
+
+// console.log(val2);
+// console.log(val1);
+
+// Ternary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 80
+
+iceTeaPrice>=100 ? console.log("iceTeaPrice is greater than equal to 100") : console.log("iceTeaPrice is less than 100");
+
