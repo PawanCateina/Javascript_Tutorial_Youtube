0ed0b59852140c107d9712a92330fe3aadf9f5e4
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2025-01-18 08:36:59 +0000

    04ControlFlow_Ifelse_statement

---
+++ 04controlflow/01ifelse.js
@@ -0,0 +1,63 @@
+//if statement
+
+//<,>,<=,>=,!=,==,===
+
+// if(2==="2"){
+//     console.log("true");
+     
+// }
+// else{
+//     console.log("false");
+// }
+
+
+// let temperature=20
+
+// if (temperature == 21) {
+//     console.log("less than or equal to 20");
+// }else{
+//     console.log("greater than 20");
+// }
+
+// const score=200
+
+// if (score<100) {
+//     var power = "fly"
+//     console.log(`User's Power: ${power}`); }
+// // }else{
+// //     console.log("User Power: No Power");
+// // }
+// console.log(`User Power: ${power}`);
+
+// const balance = 510
+// //implicit scope
+// // if (balance > 500) console.log('balance is greater than 500');
+
+// if (balance < 500) {
+//     console.log("balance is less than 500");    
+// } else if(balance < 750){
+//     console.log("balance is less than 750");
+// }else{
+//     console.log("balance is less than 1200");
+// }
+
+
+
+// const userLoggedIn = true
+// const userDebitCard = true
+
+// if (userLoggedIn && userDebitCard) {
+//     console.log("Allow to buy courses");
+// }else{
+//     console.log("Sorry please login and add debit card in your account");
+// }
+
+// const loggedInGoogle = false
+// const loggedInEmail = false
+
+// if (loggedInGoogle || loggedInEmail) {
+//     console.log("User Logged In");
+    
+// } else {
+//     console.log("Login with atleast with Google or Email");
+// }
