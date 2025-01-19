71f81117416eef0c7af875efe1a43bf7422f9324
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2025-01-19 10:34:47 +0000

    01forloop

--- 05iterations.js/01for.js
+++ 05iterations.js/01for.js
@@ -36,10 +36,10 @@
 // }
 // console.log("Out of Loop");
 
-for (let i = 1; i < 10; i++) {
+// for (let i = 1; i < 10; i++) {
     
-    if (i == 5) {
-        continue;
-    }   
-    console.log(`Number is: ${i}`);
-}
+//     if (i == 5) {
+//         continue;
+//     }   
+//     console.log(`Number is: ${i}`);
+// }
