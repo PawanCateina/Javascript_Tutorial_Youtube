ecb94781a9823cda29d14d3a5d872b18cd561232
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2025-01-19 10:33:52 +0000

    01forlops

---
+++ 05iterations.js/01for.js
@@ -0,0 +1,45 @@
+// for loops
+
+// for (let j = 0; index <= 10; index++) {
+//     const element = index
+//     if (element == 5) {
+//         // console.log("5 is the best number");
+//     }
+//     // console.log(index);
+// }
+
+// Outer and Inner Loop
+
+// Tables Printing Program
+// for (let i = 1; i <=10; i++) {
+//     // console.log(`Table of ${i}`);
+//     for (let j = 1; j <=10 ; j++) {
+//         // console.log(`${i} * ${j} = ${i*j} `);
+//     }
+// }
+
+// const myAry = ["Dhoni","Virat","Rohit","Pandya","Shikhar"]
+
+// for (let i = 0; i < myAry.length; i++) {
+//     const element = myAry[i];
+//     console.log(element);
+// }
+
+// break and continue
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(`Number is: ${i}`);
+//     if (i == 5) {
+//         break;
+//     }   
+    
+// }
+// console.log("Out of Loop");
+
+for (let i = 1; i < 10; i++) {
+    
+    if (i == 5) {
+        continue;
+    }   
+    console.log(`Number is: ${i}`);
+}
