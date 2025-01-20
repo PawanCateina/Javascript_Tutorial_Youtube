4567d378a64e7de7399aaca14519bc6bba6dffc6
Pawan Jinaga <127372000+PawanCateina@users.noreply.github.com>
2025-01-20 07:01:42 +0000

    02wile_dowhileLoops.js

---
+++ 05iterations.js/02dowhile.js
@@ -0,0 +1,19 @@
+// While loop
+
+// i=1
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+// console.log("Out of loop");
+
+//do while loop
+
+let i=0
+
+do {
+    console.log(`Value is: ${i}`);
+    i+2;
+} while (i<=10)
+
+console.log("Out of loop");
